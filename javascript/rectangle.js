4c1e5e22619405b45eb88d618c6aae7ca59db9c9
sQKhanhs <innespham@gmail.com>
2023-03-06 16:28:14 +0700

    p

---
+++ javascript/rectangle.js
@@ -0,0 +1,9 @@
+let inputwidth;
+let inputheight;
+
+inputwidth= prompt("Nhập chiều dài của hình chữ nhật:");
+inputheight= prompt("Nhập chiều cao của hình chữ nhật:");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area= width*height
+document.write("Diện tích là:" + area)
