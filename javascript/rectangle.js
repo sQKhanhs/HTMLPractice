77a63f494f74aa5c522ff408fd14035fd50df60a
sQKhanhs <innespham@gmail.com>
2023-03-07 08:58:38 +0700

    check

--- javascript/rectangle.js
+++ javascript/rectangle.js
@@ -5,5 +5,5 @@ inputwidth= prompt("Nhập chiều dài của hình chữ nhật:");
 inputheight= prompt("Nhập chiều cao của hình chữ nhật:");
 let width = parseInt(inputwidth);
 let height = parseInt(inputheight);
-let area= width*height
+let area= width*height*Math.PI
 document.write("Diện tích là:" + area)
